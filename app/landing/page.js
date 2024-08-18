0d593282eb7d835a5b44c2f4f0d0d51738aaa57b
Pranav <chhabrapranav2001@gmail.com>
2024-08-18 16:40:50 -0500

    added the facial recognition check

--- app/landing/page.js
+++ app/landing/page.js
@@ -1,5 +1,29 @@
+"use client"
+
+import React, { useState, useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase"; // Update this to your firebase config path
 import LandingPage from "../components/LandingPage";
 
 export default function Landing() {
-    return <LandingPage />;
-  }
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const { user } = useUser();
+
+  useEffect(() => {
+    const fetchSubscriptionStatus = async () => {
+      if (user) {
+        const userRef = doc(db, "users", user.id);
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setIsSubscribed(userData.isSubscribed || false); // Assuming the subscription status is stored as `isSubscribed`
+        }
+      }
+    };
+
+    fetchSubscriptionStatus();
+  }, [user]);
+
+  return <LandingPage isSubscribed={isSubscribed} />;
+}
