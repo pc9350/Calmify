a6be6fee802ed737e747590d157f12ea5cc853dc
Pranav <chhabrapranav2001@gmail.com>
2024-08-19 15:27:26 -0500

    changed model

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -39,7 +39,7 @@ export async function POST(req) {
                 { role: "system", content: systemAndFlashcardPrompt },
                 { role: "user", content: data }
             ],
-            model: "gpt-3.5-turbo"
+            model: "gpt-4-turbo"
         });
 
         let flashcardsResponse;
